1aed8d88b358dd72ffc903b69e5f397c526fc622
Julien Martinez <julien.martinez45@free.fr>
2013-01-19 14:26:22 +0100

    optimisation du code + gestion mouvement totem
    création de la class Joueurs
    mouvement des totems limité à 3

--- js/script.js
+++ js/script.js
@@ -13,98 +13,22 @@ $(function(){
 	nouvellePartie();
 });
 
-function joueurTT(){
-//Fonction click des TT et du totem BN
-	var focus;
-	$(".TT, #BN").click(function () 
-	{
-		var myClass = $(this).attr("class").split(' ');
- 		var myId = $(this).closest("tr").attr("id");
-		$(this).addClass("select");
-		
-		//Cases libres en blanc
-		for (i = 0; i < 7; i++)
-		{
-			for(j = 0; j < 10; j++)
-			{
-				resClass = $("#plateau tr#"+ i + " td."+j).attr("class").split(' ');
-				resId = $("#plateau tr#"+ i + " td."+j).attr("id");
-				if(!resId && !resClass[1])
-				{
-					$(".C").off('click');
-					$(".C").removeClass('C');
-				}
-			}
-		}
-		
-		
-		//Si y'a une deuxième classe (TT)
-		if(myClass[1])
-		{
-			for (var i = 0; i < 3; i++)
-			{
-				for (var y = 0; y < 3; y++)
-				{
-					var curID = (myId - 1 + i);
-					var curClass= (myClass[0] - 1 + y);
-					if(curID != myId || curClass != myClass[0])
-					{
-						var attr = $("tr#"+curID+" td."+curClass).attr("class").split(' ');
-						var id = $("tr#"+curID+" td."+curClass).attr("id");
-						if(!attr[1] && !id)
-						{
-							$("tr#"+curID+" td."+curClass).addClass('C');
-						}
-					}
-				}
-			}
-		}
-		//Sinon (BN)
-		else
-		{
-			for (var i = 0; i < 5; i++)
-			{
-				for (var y = 0; y < 5; y++)
-				{
-					var curID = (myId - 2 + i);
-					var curClass= (myClass[0] - 2 + y);
-					if(curID != myId || curClass != myClass[0])
-					{
-						if (curID >= 0 && curClass >= 0)
-						{
-							var attr = $("tr#"+curID+" td."+curClass).attr("class").split(' ');
-							var id = $("tr#"+curID+" td."+curClass).attr("id");
-							if(!attr[1] && !id)
-							{
-								$("tr#"+curID+" td."+curClass).addClass('C');
-							}
-						}
-					}
-				}
-			}
-		}
-		
-		if(focus)
-		{
-			if (focus.attr('id') == "BN")
-				$(focus).removeClass("select");
-			else
-				$(focus).removeClass("select");
-		}
-		focus = $(this);
-		
-		//fonction click des cases de déplacement
-		$(".C").click(function () 
-		{
-			deplacement(focus,$(this),1);
-		});
-	});
+// classe joueur
+function Joueurs(id,pion,totem,mvTotem) {
+	this.id=id;
+	this.pion=pion;
+	this.totem=totem;
+	this.mvTotem=mvTotem;
 }
+var j=1;
 
-function joueurEM(){
-//Fonction click des EM et du totem EH
+function jouerTour(ja,jp){ //ja = joueur actif - jp = joueur passif
+//Fonction click des pions et du totem
 	var focus;
-	$(".EM, #EH").click(function () 
+	var option = "."+ja.pion+", #"+ja.totem ;
+	if(ja.mvTotem == 0)
+		option = "."+ja.pion;
+	$(option).click(function () 
 	{
 		var myClass = $(this).attr("class").split(' ');
  		var myId = $(this).closest("tr").attr("id");
@@ -126,7 +50,7 @@ function joueurEM(){
 		}
 		
 		
-		//Si y'a une deuxième classe (EM)
+		//Si y'a une deuxième classe (pion)
 		if(myClass[1])
 		{
 			for (var i = 0; i < 3; i++)
@@ -147,7 +71,7 @@ function joueurEM(){
 				}
 			}
 		}
-		//Sinon (EH)
+		//Sinon (totem)
 		else
 		{
 			for (var i = 0; i < 5; i++)
@@ -174,7 +98,7 @@ function joueurEM(){
 		
 		if(focus)
 		{
-			if (focus.attr('id') == "EH")
+			if (focus.attr('id') == ja.totem)
 				$(focus).removeClass("select");
 			else
 				$(focus).removeClass("select");
@@ -184,12 +108,17 @@ function joueurEM(){
 		//fonction click des cases de déplacement
 		$(".C").click(function () 
 		{
-			deplacement(focus,$(this),2);
+			deplacement(focus,$(this),ja);
+			jouerPartie(jp,ja);
 		});
 	});
+	
 }
 
 function nouvellePartie(){
+	//initialisation des joueurs
+	joueur1=new Joueurs(1,'TT','BN',3);
+	joueur2=new Joueurs(2,'EM','EH',3);
 
 	//Ajout des id des totems
 	$("#plateau tr#3 td.0").attr('id', "BN");
@@ -201,44 +130,23 @@ function nouvellePartie(){
 		$("#plateau tr#"+ t +" td.1").addClass("TT");
 		$("#plateau tr#"+ t +" td.8").addClass("EM");
 	}
-	jouerTour(1);
+	jouerPartie(joueur1,joueur2);
 }
 
-function jouerTour(joueur){
-
-	if(joueur == 1){ //tour du joueur 1 (TT)
-		console.log("joueur 1");
-		joueurTT();
-	}
-	else{ //tour du joueur 2 (EM)
-		console.log("joueur 2");
-		joueurEM();
-	}
+function jouerPartie(j1,j2){
+	jouerTour(j1,j2);
 }
 
 function deplacement(pion,choix,joueur){
 	resId = $(pion).attr("id");//si le pion est un totem
-	if(joueur == 1){
 		if(resId){//déplacement du totem
-			$(choix).removeClass('C').attr('id','BN');
+			$(choix).removeClass('C').attr('id',joueur.totem);
 			$(pion).removeClass('select').removeAttr('id').addClass('C');
+			joueur.mvTotem--;
 		}else{//déplacement du pion
-			$(choix).removeClass('C').addClass('TT');
-			$(pion).removeClass('TT select').addClass('C');
+			$(choix).removeClass('C').addClass(joueur.pion);
+			$(pion).removeClass(joueur.pion+' select').addClass('C');
 		}
-		$(".C, .TT, #BN").off('click');
-		joueur=2;
-	}else{
-		if(resId){//déplacement du totem
-			$(choix).removeClass('C').attr('id','EH');
-			$(pion).removeClass('select').removeAttr('id').addClass('C');
-		}else{//déplacement du pion
-			$(choix).removeClass('C').addClass('EM');
-			$(pion).removeClass('EM select').addClass('C');
-		}
-		$(".C, .EM, #EH").off('click');
-		joueur=1;
-	}
-	$(".C").removeClass('C');
-	jouerTour(joueur); //tour du joueur suivant
+		$(".C, ."+joueur.pion+", #"+joueur.totem).off('click');
+		$(".C").removeClass('C');
 }
